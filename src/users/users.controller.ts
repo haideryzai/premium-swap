c3670d3c6f1b2d41f40decba6d7f383da6027981
Haider Zaman <hzaman@code-freaks.com>
2024-05-17 17:17:00 +0500

    added patch

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common';
-import { Get, Param } from '@nestjs/common';
+import { Get, Patch, Param } from '@nestjs/common';
 
 // service functions
 import { UsersService } from './users.service';
@@ -12,8 +12,14 @@ export class UsersController {
     return this.usersService.getAllUsers();
   }
 
-  @Get()
+  @Get('/:id')
   getUserById(@Param('id') id: number) {
+    console.log(id);
     return this.usersService.getUserById(id);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: number) {
+    return this.usersService.updateUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
+// db
+const users = [
+  { id: 1, firstName: 'John', lastName: 'Doe' },
+  { id: 2, firstName: 'Jane', lastName: 'Doe' },
+  { id: 3, firstName: 'Bob', lastName: 'Doe' },
+];
+
 @Injectable()
 export class UsersService {
   public getAllUsers() {
-    const users = [
-      { id: 1, firstName: 'John', lastName: 'Doe' },
-      { id: 2, firstName: 'Jane', lastName: 'Doe' },
-    ];
     return users;
   }
 
   public getUserById(id: number) {
-    const users = [
-      { id: 1, firstName: 'John', lastName: 'Doe' },
-      { id: 2, firstName: 'Jane', lastName: 'Doe' },
-    ];
-    return users.find((user) => user.id === id);
+    return users.filter((user) => {
+      return user.id == id;
+    });
+  }
+
+  public updateUser(id: number) {
+    const theUser = users.filter((user) => {
+      return user.id == id;
+    });
+    theUser[0].firstName = 'updated';
+    return theUser;
   }
 }
